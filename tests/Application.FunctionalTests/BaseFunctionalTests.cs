f4e5bf30575c88016e0013dcbf64a90152b95606
Michal Gajda <michal@gajda.co.uk>
2025-02-24 00:03:32 +0000

    Add UserSecrets

--- tests/Application.FunctionalTests/BaseFunctionalTests.cs
+++ tests/Application.FunctionalTests/BaseFunctionalTests.cs
@@ -6,12 +6,14 @@ using PubLink.Infrastructure;
 
 public abstract class BaseFunctionalTests
 {
+    const string USER_SECRETS_ID = "0f23e17d-ea8d-4b5a-98a8-72763297bbf9";
     protected readonly ServiceProvider serviceProvider;
 
     protected BaseFunctionalTests()
     {
         var configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
+            .AddUserSecrets(USER_SECRETS_ID)
             .Build();
 
         var services = new ServiceCollection();
